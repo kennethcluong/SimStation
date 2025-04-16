9292b14718c0ec881efa755ae010c357ba24d12d
bhagatjeetd <dbhagatjeet@gmail.com>
2025-04-15 18:08:12 -0700

    Delete SimStationMain.java

--- SimStationMain.java
+++
@@ -1,9 +0,0 @@
-package SimStation;
-
-public class SimStationMain {
-    public static void main(String[] args) {
-        SimStationFactory factory = new SimStationFactory();
-        WorldPanel panel = new WorldPanel(factory);
-        panel.display();
-    }
-}
